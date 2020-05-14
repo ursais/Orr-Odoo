e146a55a1f99d4401fbc06fcb6f825822421e6f5
smangukiya <smangukiya>
2020-05-14 23:07:00 +0000

    [REM]Final Clean up and Link-Addons enable

--- src/create_link.sh
+++ src/create_link.sh
@@ -1,49 +1,11 @@
 #! /bin/sh
 cd link-addons
 
-rm avatax_connector
-ln -s ../avatax_connector/avatax_connector avatax_connector
-
 rm stock_analytic
 ln -s ../account-analytic/stock_analytic stock_analytic
 
-rm account_banking_reconciliation
-ln -s ../account-reconcile/account_banking_reconciliation account_banking_reconciliation
-
-rm account_invoice_transmit_method
-ln -s ../account-invoicing/account_invoice_transmit_method account_invoice_transmit_method
-
-rm partner_time_to_pay
-ln -s ../account-invoice-reporting/partner_time_to_pay partner_time_to_pay
-
 rm partner_aging
-rm account_payment_credit_card
 ln -s ../account-payment/partner_aging partner_aging
-ln -s ../account-payment/account_payment_credit_card account_payment_credit_card
-
-rm account_payment_partner
-rm account_banking_mandate
-rm account_payment_purchase
-rm account_payment_sale
-rm account_payment_mode
-ln -s ../bank-payment/account_payment_partner account_payment_partner
-ln -s ../bank-payment/account_banking_mandate account_banking_mandate
-ln -s ../bank-payment/account_payment_purchase account_payment_purchase
-ln -s ../bank-payment/account_payment_sale account_payment_sale
-ln -s ../bank-payment/account_payment_mode account_payment_mode
-
-rm contract
-rm agreement
-rm agreement_legal
-rm contract_sale
-rm contract_mandate
-rm contract_payment_mode
-ln -s ../contract/contract contract
-ln -s ../contract/agreement agreement
-ln -s ../contract/agreement_legal agreement_legal
-ln -s ../contract/contract_sale contract_sale
-ln -s ../contract/contract_mandate contract_mandate
-ln -s ../contract/contract_payment_mode contract_payment_mode
 
 rm fieldservice
 rm fieldservice_account
@@ -57,12 +19,10 @@ rm fieldservice_project
 rm fieldservice_account_analytic
 rm fieldservice_crm
 rm fieldservice_vehicle
-#rm fieldservice_vehicle_stock
+rm fieldservice_vehicle_stock
 rm fieldservice_delivery
 rm fieldservice_activity
 rm fieldservice_location_builder
-#rm fieldservice_analytic_location_builder
-#rm fieldservice_stock_location_builder
 ln -s ../field-service/fieldservice fieldservice
 ln -s ../field-service/fieldservice_account fieldservice_account
 ln -s ../field-service/fieldservice_purchase fieldservice_purchase
@@ -75,12 +35,10 @@ ln -s ../field-service/fieldservice_project fieldservice_project
 ln -s ../field-service/fieldservice_account_analytic fieldservice_account_analytic
 ln -s ../field-service/fieldservice_crm fieldservice_crm
 ln -s ../field-service/fieldservice_vehicle fieldservice_vehicle
-# ln -s ../field-service/fieldservice_vehicle_stock fieldservice_vehicle_stock
+ln -s ../field-service/fieldservice_vehicle_stock fieldservice_vehicle_stock
 ln -s ../field-service/fieldservice_delivery fieldservice_delivery
 ln -s ../field-service/fieldservice_activity fieldservice_activity
 ln -s ../field-service/fieldservice_location_builder fieldservice_location_builder
-#ln -s ../field-service/fieldservice_analytic_location_builder fieldservice_analytic_location_builder
-#ln -s ../field-service/fieldservice_stock_location_builder fieldservice_stock_location_builder
 
 rm base_geoengine
 rm base_google_map
@@ -148,9 +106,3 @@ rm web_timeline
 rm web_environment_ribbon
 ln -s ../web/web_timeline web_timeline
 ln -s ../web/web_environment_ribbon web_environment_ribbon
-
-rm  sale_commission
-ln -s ../commission/sale_commission sale_commission
-
-
-#ln -s ../temp-addons/fieldservice_vehicle_stock fieldservice_vehicle_stock
