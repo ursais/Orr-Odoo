b209b75eb8c8c10590a2a1054dc9ebda2c5ac970
scampbell <scampbell@opensourceintegrators.com>
2020-07-16 16:35:25 -0700

    [IMP] Improved Workflow for ORR
    [IMP] Flow Service Profile to FSM Order
    
    [DEL] Unwanted Line
    
    [IMP] Rework and Move Modules
    
    [IMP] Fix Field
    
    [IMP] Equipment Attrs Invisible

--- src/custom-addons/osi_orr_recurring_workflow/models/sale.py
+++ src/custom-addons/osi_orr_recurring_workflow/models/sale.py
@@ -25,6 +25,34 @@ class SaleOrder(models.Model):
                                 line_id.fsm_equipment_id.serviceprofile_id = sp_id
         return res
 
+    @api.multi
+    def action_confirm(self):
+        result = super(SaleOrder, self).action_confirm()
+        for so in self:
+            project = self.env['project.project'].search([
+                ('sale_order_id', '=', so.id)], limit=1)
+            if project:
+                for recurring_id in self.fsm_recurring_ids:
+                    recurring_id.project_id = project
+                    for task_id in self.tasks_ids:
+                        if task_id.sale_line_id == recurring_id.sale_line_id:
+                            recurring_id.task_id = task_id
+                    for line_id in self.agreement_id.serviceprofile_ids:
+                        if line_id.product_id == recurring_id.sale_line_id.product_id.product_tmpl_id:
+                            recurring_id.serviceprofile_id = line_id
+            for recurring_id in self.fsm_recurring_ids:
+                for order_id in recurring_id.fsm_order_ids:
+                    if recurring_id.agreement_id:
+                        order_id.write({'agreement_id': recurring_id.agreement_id.id})
+                    if recurring_id.serviceprofile_id:
+                        order_id.write({'serviceprofile_id': recurring_id.serviceprofile_id.id})
+                    if recurring_id.fsm_equipment_id:
+                        order_id.write({'equipment_id': recurring_id.fsm_equipment_id.id})
+                    if recurring_id.project_id:
+                        order_id.write({'project_id': recurring_id.project_id.id})
+                    if recurring_id.task_id:
+                        order_id.write({'project_task_id': recurring_id.task_id.id})
+        return result
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
